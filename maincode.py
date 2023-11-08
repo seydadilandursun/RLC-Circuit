2ab874f7b801799826a8cb27cbba619a8e0b060b
Şeyda Dilan Dursun <108757360+seydadilandursun@users.noreply.github.com>
2023-11-08 21:39:14 +0300

    Update maincode.py
    Last one

--- maincode.py
+++ maincode.py
@@ -1,22 +1,22 @@
 import math
 
 # Define the values of R, L, C, and frequency (f)
-R1 =float(input("Enter R(ohm): ")) # Resistance in ohms
-L1 = float(input("Enter L(henrys): ")) # Inductance in henrys
-C1 = float(input("Enter C(farads): ")) # Capacitance in farads
-f1 = float(input("Enter f(Hertz): ")) # Frequency in Hertz
+r_1 = float(input("Enter R(ohm): ")) # Resistance in ohms
+l_1 = float(input("Enter L(henrys): ")) # Inductance in henrys
+c_1 = float(input("Enter C(farads): ")) # Capacitance in farads
+f_1 = float(input("Enter f(Hertz): ")) # Frequency in Hertz
 
-def calculate_impedance(R, L, C, f):
+def calculate_impedance(r, l, c, f):
         # Calculate inductive reactance (X_L)
-        X_L = 2 * math.pi * f * L
+        X_l = 2 * math.pi * f * l
         
         # Calculate capacitive reactance (X_C), handle potential division by zero
-        X_C = 1 / (2 * math.pi * f * C)
+        X_c = 1 / (2 * math.pi * f * C)
         
         # Calculate impedance (Z)
-        Z = math.sqrt(R**2 + (X_L - X_C)**2)
+        Z = math.sqrt(r**2 + (X_l - X_c)**2)
         return Z
 
-Z_total=calculate_impedance(R1,L1,C1,f1) 
+Z_total = calculate_impedance(r_1,l_1,c_1,f_1) 
 
 print(f"Impedance (Z) = {Z_total} ohms")
